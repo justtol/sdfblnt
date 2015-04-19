e2d43926400dbb697806394592de01d5cd615dba
justtol <justfortol@gmail.com>
2015-04-19 20:43:58 +0300

    fixed city

--- ActiveCRMData/src/main/java/com/active/activecrm/data/entities/dicts/City.java
+++ ActiveCRMData/src/main/java/com/active/activecrm/data/entities/dicts/City.java
@@ -1,25 +1,31 @@
 package com.active.activecrm.data.entities.dicts;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
  * справочник городов
  */
 @Entity
-@Table( name = "REGION" )
+@Table( name = "CITY" )
 public class City extends BaseDictionary implements Serializable
 {
 
     /**
      * тип населенного пункта
      */
+    @Column( name = "CITY_TYPE" )
     public String type;
+
     /**
      * регион
      */
-    public String regioncode;
+    @Column( name = "REGION_CODE" )
+    public String regionCode;
+
+    @ManyToOne( fetch = FetchType.LAZY )
+    @JoinColumn( name = "REGION_CODE", insertable = false, updatable = false )
+    private Region region;
 
 
 }
