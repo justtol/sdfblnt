01dbe2cc64f86c5348174da9c752e09410a5b319
justtol <justfortol@gmail.com>
2015-04-19 20:44:33 +0300

    fixed city

--- ActiveCRMData/src/main/java/com/active/activecrm/data/entities/dicts/City.java
+++ ActiveCRMData/src/main/java/com/active/activecrm/data/entities/dicts/City.java
@@ -27,5 +27,33 @@ public class City extends BaseDictionary implements Serializable
     @JoinColumn( name = "REGION_CODE", insertable = false, updatable = false )
     private Region region;
 
+    public String getType()
+    {
+        return type;
+    }
 
+    public void setType( String type )
+    {
+        this.type = type;
+    }
+
+    public String getRegionCode()
+    {
+        return regionCode;
+    }
+
+    public void setRegionCode( String regionCode )
+    {
+        this.regionCode = regionCode;
+    }
+
+    public Region getRegion()
+    {
+        return region;
+    }
+
+    public void setRegion( Region region )
+    {
+        this.region = region;
+    }
 }
