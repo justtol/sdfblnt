1c7bfaae3ebf9ca9e296d31f69fb04edc178dcea
justtol <justfortol@gmail.com>
2015-05-10 17:00:04 +0300

    ext js cleaning

---
+++ ActiveCRMWebApp/src/main/webapp/app.js
@@ -0,0 +1,19 @@
+/*
+ * This file is generated and updated by Sencha Cmd. You can edit this file as
+ * needed for your application, but these edits will have to be merged by
+ * Sencha Cmd when upgrading.
+ */
+Ext.application({
+    name: 'ActiveCrmApp',
+    //appFolder: 'js/application/app',
+    //appDir:'js/application/app',
+    extend: 'ActiveCrmApp.Application',
+    
+    autoCreateViewport: 'ActiveCrmApp.view.main.Main'
+	
+    //-------------------------------------------------------------------------
+    // Most customizations should be made to ActiveCrmApp.Application. If you need to
+    // customize this file, doing so below this section reduces the likelihood
+    // of merge conflicts when upgrading to new versions of Sencha Cmd.
+    //-------------------------------------------------------------------------
+});
